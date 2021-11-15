2bb400ea94a3ad9228db7496b07fb7551792b0e2
Rostiks52 <ksyu_05@mail.ru>
2021-11-15 14:15:48 +0300

    Allure-dir added

--- src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -1,7 +1,7 @@
 package ru.netology.delivery.test;
 
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
